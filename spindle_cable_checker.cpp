4eebb39945e62b3d09aca107b3fb37c2060ba8b1
lozprocter <lozprocter@yahoo.co.uk>
2023-01-12 21:45:26 +0000

    typos fixed

--- spindle_cable_checker.cpp
+++ spindle_cable_checker.cpp
@@ -188,12 +188,12 @@ void loop() {
             }else{
                 Serial.println("Cable 1 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_1_sc == HIGH){
+            Serial.println("Cable 1 SC: PASS")
+            }else if (val_cable_1_sc == LOW){
+                Serial.println("Cable 1 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 1 SC: No Result")
             }
         
         //cable 2
@@ -218,252 +218,252 @@ void loop() {
             }else{
                 Serial.println("Cable 2 OL: No Result")
             }
-        if (val_cable_2_gnd == HIGH){
-            Serial.println("Cable 2 GND: PASS")
-            }else if (val_cable_2_gnd == LOW){
-                Serial.println("Cable 2 GND: FAIL")
+        if (val_cable_2_sc == HIGH){
+            Serial.println("Cable 2 SC: PASS")
+            }else if (val_cable_2_sc == LOW){
+                Serial.println("Cable 2 SC: FAIL")
             }else{
-                Serial.println("Cable 2 GND: No Result")
+                Serial.println("Cable 2 SC: No Result")
             }
 
         //cable 3
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_3_24v == HIGH){
+            Serial.println("Cable 3 24V: PASS")
+            }else if (val_cable_3_24v == LOW){
+                Serial.println("Cable 3 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 3 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_3_gnd == HIGH){
+            Serial.println("Cable 3 GND: PASS")
+            }else if (val_cable_3_gnd == LOW){
+                Serial.println("Cable 3 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 3 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_3_ol == HIGH){
+            Serial.println("Cable 3 OL: PASS")
+            }else if (val_cable_3_ol == LOW){
+                Serial.println("Cable 3 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 3 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_3_sc == HIGH){
+            Serial.println("Cable 3 SC: PASS")
+            }else if (val_cable_3_sc == LOW){
+                Serial.println("Cable 3 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 3 SC: No Result")
             }
 
         //cable 4
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_4_24v == HIGH){
+            Serial.println("Cable 4 24V: PASS")
+            }else if (val_cable_4_24v == LOW){
+                Serial.println("Cable 4 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 4 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_4_gnd == HIGH){
+            Serial.println("Cable 4 GND: PASS")
+            }else if (val_cable_4_gnd == LOW){
+                Serial.println("Cable 4 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 4 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_4_ol == HIGH){
+            Serial.println("Cable 4 OL: PASS")
+            }else if (val_cable_4_ol == LOW){
+                Serial.println("Cable 4 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 4 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_4_sc == HIGH){
+            Serial.println("Cable 4 SC: PASS")
+            }else if (val_cable_4_sc == LOW){
+                Serial.println("Cable 4 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 4 SC: No Result")
             }
 
         //cable 5
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_5_24v == HIGH){
+            Serial.println("Cable 5 24V: PASS")
+            }else if (val_cable_5_24v == LOW){
+                Serial.println("Cable 5 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 5 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_5_gnd == HIGH){
+            Serial.println("Cable 5 GND: PASS")
+            }else if (val_cable_5_gnd == LOW){
+                Serial.println("Cable 5 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 5 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_5_ol == HIGH){
+            Serial.println("Cable 5 OL: PASS")
+            }else if (val_cable_5_ol == LOW){
+                Serial.println("Cable 5 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 5 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_5_sc == HIGH){
+            Serial.println("Cable 5 SC: PASS")
+            }else if (val_cable_5_sc == LOW){
+                Serial.println("Cable 5 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 5 SC: No Result")
             }
 
         //cable 6
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_6_24v == HIGH){
+            Serial.println("Cable 6 24V: PASS")
+            }else if (val_cable_6_24v == LOW){
+                Serial.println("Cable 6 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 6 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_6_gnd == HIGH){
+            Serial.println("Cable 6 GND: PASS")
+            }else if (val_cable_6_gnd == LOW){
+                Serial.println("Cable 6 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 6 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_6_ol == HIGH){
+            Serial.println("Cable 6 OL: PASS")
+            }else if (val_cable_6_ol == LOW){
+                Serial.println("Cable 6 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 6 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_6_sc == HIGH){
+            Serial.println("Cable 6 SC: PASS")
+            }else if (val_cable_6_sc == LOW){
+                Serial.println("Cable 6 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 6 SC: No Result")
             }
 
         //cable 7
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_7_24v == HIGH){
+            Serial.println("Cable 7 24V: PASS")
+            }else if (val_cable_7_24v == LOW){
+                Serial.println("Cable 7 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 7 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_7_gnd == HIGH){
+            Serial.println("Cable 7 GND: PASS")
+            }else if (val_cable_7_gnd == LOW){
+                Serial.println("Cable 7 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 7 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_7_ol == HIGH){
+            Serial.println("Cable 7 OL: PASS")
+            }else if (val_cable_7_ol == LOW){
+                Serial.println("Cable 7 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 7 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_7_sc == HIGH){
+            Serial.println("Cable 7 SC: PASS")
+            }else if (val_cable_7_sc == LOW){
+                Serial.println("Cable 7 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 7 SC: No Result")
             }
 
         //cable 8
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_8_24v == HIGH){
+            Serial.println("Cable 8 24V: PASS")
+            }else if (val_cable_8_24v == LOW){
+                Serial.println("Cable 8 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 8 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_8_gnd == HIGH){
+            Serial.println("Cable 8 GND: PASS")
+            }else if (val_cable_8_gnd == LOW){
+                Serial.println("Cable 8 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 8 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_8_ol == HIGH){
+            Serial.println("Cable 8 OL: PASS")
+            }else if (val_cable_8_ol == LOW){
+                Serial.println("Cable 8 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 8 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_8_sc == HIGH){
+            Serial.println("Cable 8 SC: PASS")
+            }else if (val_cable_8_sc == LOW){
+                Serial.println("Cable 8 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 8 SC: No Result")
             }
 
         //cable 9
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_9_24v == HIGH){
+            Serial.println("Cable 9 24V: PASS")
+            }else if (val_cable_9_24v == LOW){
+                Serial.println("Cable 9 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 9 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_9_gnd == HIGH){
+            Serial.println("Cable 9 GND: PASS")
+            }else if (val_cable_9_gnd == LOW){
+                Serial.println("Cable 9 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 9 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_9_ol == HIGH){
+            Serial.println("Cable 9 OL: PASS")
+            }else if (val_cable_9_ol == LOW){
+                Serial.println("Cable 9 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 9 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_9_sc == HIGH){
+            Serial.println("Cable 9 SC: PASS")
+            }else if (val_cable_9_sc == LOW){
+                Serial.println("Cable 9 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 9 SC: No Result")
             }
 
         //cable 10
-        if (val_cable_1_24v == HIGH){
-            Serial.println("Cable 1 24V: PASS")
-            }else if (val_cable_1_24v == LOW){
-                Serial.println("Cable 1 24V: FAIL")
+        if (val_cable_10_24v == HIGH){
+            Serial.println("Cable 10 24V: PASS")
+            }else if (val_cable_10_24v == LOW){
+                Serial.println("Cable 10 24V: FAIL")
             }else{
-                Serial.println("Cable 1 24V: No Result")
+                Serial.println("Cable 10 24V: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_10_gnd == HIGH){
+            Serial.println("Cable 10 GND: PASS")
+            }else if (val_cable_10_gnd == LOW){
+                Serial.println("Cable 10 GND: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 10 GND: No Result")
             }
-        if (val_cable_1_ol == HIGH){
-            Serial.println("Cable 1 OL: PASS")
-            }else if (val_cable_1_ol == LOW){
-                Serial.println("Cable 1 OL: FAIL")
+        if (val_cable_10_ol == HIGH){
+            Serial.println("Cable 10 OL: PASS")
+            }else if (val_cable_10_ol == LOW){
+                Serial.println("Cable 10 OL: FAIL")
             }else{
-                Serial.println("Cable 1 OL: No Result")
+                Serial.println("Cable 10 OL: No Result")
             }
-        if (val_cable_1_gnd == HIGH){
-            Serial.println("Cable 1 GND: PASS")
-            }else if (val_cable_1_gnd == LOW){
-                Serial.println("Cable 1 GND: FAIL")
+        if (val_cable_10_sc == HIGH){
+            Serial.println("Cable 10 SC: PASS")
+            }else if (val_cable_10_sc == LOW){
+                Serial.println("Cable 10 SC: FAIL")
             }else{
-                Serial.println("Cable 1 GND: No Result")
+                Serial.println("Cable 10 SC: No Result")
             }
 
             
