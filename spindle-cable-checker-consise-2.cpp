36d834c8992ec1e519b0b3f0aa973ec7cf5ea5d7
Loz Procter <lozprocter@yahoo.co.uk>
2023-01-16 09:53:46 +0000

    logic fix

--- spindle-cable-checker-consise-2.cpp
+++ spindle-cable-checker-consise-2.cpp
@@ -58,30 +58,31 @@ const int cable_10_gnd = 38;
 const int cable_10_ol = 39;
 const int cable_10_sc = 40;
 
-//run counter to stop while loop
-int run_counter = 0;
 //list of pins with assigned names
 int pins[40] = {cable_1_24v, cable_1_gnd, cable_1_ol, cable_1_sc, cable_2_24v, cable_2_gnd,  cable_2_ol, cable_2_sc, cable_3_24v, cable_3_gnd,  cable_3_ol, cable_3_sc, cable_4_24v, cable_4_gnd, cable_4_ol, cable_4_sc, cable_5_24v, cable_5_gnd, cable_5_ol, cable_5_sc, cable_6_24v, cable_6_gnd, cable_6_ol, cable_6_sc, cable_7_24v, cable_7_gnd, cable_7_ol, cable_7_sc, cable_8_24v, cable_8_gnd, cable_8_ol, cable_8_sc, cable_9_24v, cable_9_gnd, cable_9_ol, cable_9_sc, cable_10_24v, cable_10_gnd, cable_10_ol, cable_10_sc,};
+char pin_names[40] = {"cable_1_24v", "cable_1_gnd", "cable_1_ol", "cable_1_sc", "cable_2_24v", "cable_2_gnd",  "cable_2_ol", "cable_2_sc", "cable_3_24v", "cable_3_gnd",  "cable_3_ol", "cable_3_sc", "cable_4_24v", "cable_4_gnd", "cable_4_ol", "cable_4_sc", "cable_5_24v", "cable_5_gnd", "cable_5_ol", "cable_5_sc", "cable_6_24v", "cable_6_gnd", "cable_6_ol", "cable_6_sc", "cable_7_24v", "cable_7_gnd", "cable_7_ol", "cable_7_sc", "cable_8_24v", "cable_8_gnd", "cable_8_ol", "cable_8_sc", "cable_9_24v", "cable_9_gnd", "cable_9_ol", "cable_9_sc", "cable_10_24v", "cable_10_gnd", "cable_10_ol", "cable_10_sc"};
+int fails[40] = {};
+
+//run counter to stop while loop
+int run_counter = 0;
 
 void setup(){
     Serial.begin(9600);
     for (int i : pins){
         pinMode(i, INPUT);
+        fails[i] = 0;
     }
 }
 
 void loop(){
     for (int i : pins){
-        digitalRead(i);
-        if (i == HIGH){
-            Serial.print(i);
+        if (digitalRead(i) == HIGH){
+            Serial.print(pin_names[i]);
             Serial.println(": PASS");
-            }else if (i == LOW){
-                Serial.print(i);
-                Serial.println(": FAIL");
             }else{
-                Serial.print(i);
-                Serial.println(": No Result");
+                Serial.print(pin_names[i]);
+                Serial.println(": FAIL");
+                fails[i] += 1;
             }
     }
 
